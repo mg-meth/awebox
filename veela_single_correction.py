c5ae59b4dab1f1f79f45b0aa25da8d87d65c9338
mg-meth <matthias.gramlich@gmx.de>
2020-12-05 00:33:55 +0100

    bubbledancer

--- veela_single_correction.py
+++ veela_single_correction.py
@@ -26,7 +26,7 @@ def solve_succed(quality_print_results, name):
         f.write(string)
 
 
-
+"""
 name = 'TEST'
 
     # make default options object
@@ -45,7 +45,6 @@ options['user_options']['trajectory']['system_type'] = 'lift_mode'
 options['user_options']['trajectory']['lift_mode']['windings'] = 3
 options['solver']['max_iter'] = 4000
 options['solver']['max_cpu_time'] = 1.e4
-""" ### """
     #options['model']['ground_station']['ddl_t_max'] = 95.04
 
 options['user_options']['wind']['u_ref'] = 5
@@ -74,7 +73,7 @@ options['solver']['linear_solver'] = 'mumps'
 trial = awe.Trial(options, name)
 trial.build()
 trial.optimize()
-quality_print_results = trial.quality.return_results()
+#quality_print_results = trial.quality.return_results()
 trial.plot('level_3')
 trial.write_to_csv()
     #pdb.set_trace()
@@ -83,11 +82,12 @@ V_solution_scaled = trial.nlp.V(trial.optimization.solution['x'])
 print(V_final['xd', :, 'i_sd'],V_final['xd', :, 'i_sq'])
 print(V_solution_scaled['xd', :, 'i_sd'],V_solution_scaled['xd', :, 'i_sq'])
 
-solve_succed(quality_print_results, name)
+#solve_succed(quality_print_results, name)
 
 
 
-wind_ref = [2,3,4,5,6,7]
+
+wind_ref = [5,6,7]
 name = []
 
 for w in wind_ref:
@@ -109,10 +109,9 @@ for w in wind_ref:
     # trajectory should be a single pumping cycle with initial number of five windings
     options['user_options']['trajectory']['type'] = 'power_cycle'
     options['user_options']['trajectory']['system_type'] = 'lift_mode'
-    options['user_options']['trajectory']['lift_mode']['windings'] = 3
+    options['user_options']['trajectory']['lift_mode']['windings'] = 5
     options['solver']['max_iter'] = 6000
     options['solver']['max_cpu_time'] = 1.e4
-    """ ### """
     #options['model']['ground_station']['ddl_t_max'] = 95.04
 
     options['user_options']['wind']['u_ref'] = w
@@ -151,10 +150,11 @@ for w in wind_ref:
     V_solution_scaled = trial.nlp.V(trial.optimization.solution['x'])
     print(V_final['xd', :, 'i_sd'],V_final['xd', :, 'i_sq'])
     print(V_solution_scaled['xd', :, 'i_sd'],V_solution_scaled['xd', :, 'i_sq'])
+"""
 
-
+wind_ref = [7]
 for w in wind_ref:
-    name = 'single_kite_no_gen\single_kite_' + str(w) + '_log_wind'
+    name = 'single_kite_no_gen' + str(w) + '_log_wind_CORRECTION'
 
     print(name)
 
@@ -172,8 +172,8 @@ for w in wind_ref:
     options['user_options']['trajectory']['type'] = 'power_cycle'
     options['user_options']['trajectory']['system_type'] = 'lift_mode'
     options['user_options']['trajectory']['lift_mode']['windings'] = 3
-    options['solver']['max_iter'] = 6000
-    options['solver']['max_cpu_time'] = 1.e4
+    options['solver']['max_iter'] = 20000
+    options['solver']['max_cpu_time'] = 2.e4
     """ ### """
     #options['model']['ground_station']['ddl_t_max'] = 95.04
 
@@ -196,19 +196,19 @@ for w in wind_ref:
     # OPTIMIZE TRIAL #
     ##################
 
-    options['solver']['linear_solver'] = 'ma57'
+    options['solver']['linear_solver'] = 'mumps'
     #options['solver']['initialization']['fix_tether_length'] = True
 
     # initialize and optimize trial
     trial = awe.Trial(options, name)
     trial.build()
     trial.optimize()
-    quality_print_results = trial.quality.return_results()
-
+    #quality_print_results = trial.quality.return_results()
+    trial.quality.print_results()
     trial.plot('level_3')
     trial.write_to_csv()
     #pdb.set_trace()
-    solve_succed(quality_print_results, name)
+    #solve_succed(quality_print_results, name)
 
 
     V_final = trial.optimization.V_final
@@ -216,8 +216,8 @@ for w in wind_ref:
     #print(V_final['xd', :, 'i_sd'],V_final['xd', :, 'i_sq'])
     #print(V_solution_scaled['xd', :, 'i_sd'],V_solution_scaled['xd', :, 'i_sq'])
 
-
-
+"""
+wind_ref = [4,5,7]
 for w in wind_ref:
     name = 'single_kite_5_mw\single_kite_' + str(w) + '_log_wind'
 
@@ -237,10 +237,9 @@ for w in wind_ref:
     # trajectory should be a single pumping cycle with initial number of five windings
     options['user_options']['trajectory']['type'] = 'power_cycle'
     options['user_options']['trajectory']['system_type'] = 'lift_mode'
-    options['user_options']['trajectory']['lift_mode']['windings'] = 3
+    options['user_options']['trajectory']['lift_mode']['windings'] = 5
     options['solver']['max_iter'] = 6000
     options['solver']['max_cpu_time'] = 1.e4
-    """ ### """
     #options['model']['ground_station']['ddl_t_max'] = 95.04
 
     options['user_options']['wind']['u_ref'] = w
@@ -279,67 +278,4 @@ for w in wind_ref:
     V_solution_scaled = trial.nlp.V(trial.optimization.solution['x'])
     print(V_final['xd', :, 'i_sd'],V_final['xd', :, 'i_sq'])
     print(V_solution_scaled['xd', :, 'i_sd'],V_solution_scaled['xd', :, 'i_sq'])
-
-
-wind_ref = [5]
-for w in wind_ref:
-    name = 'single_boeing_125_kw_gen\single_kite_' + str(w) + '_log_wind'
-
-    print(name)
-
-
-    # make default options object
-    options = awe.Options(True)
-
-    # single kite with point-mass model
-    options['user_options']['system_model']['architecture'] = {1:0}
-    options['user_options']['system_model']['kite_dof'] = 6
-    options['user_options']['kite_standard'] = awe.boeing747_data.data_dict()
-    options['user_options']['generator'] = awe.pmsm_125_kw_gen.data_dict()
-
-    # trajectory should be a single pumping cycle with initial number of five windings
-    options['user_options']['trajectory']['type'] = 'power_cycle'
-    options['user_options']['trajectory']['system_type'] = 'lift_mode'
-    options['user_options']['trajectory']['lift_mode']['windings'] = 3
-    options['solver']['max_iter'] = 10000
-    options['solver']['max_cpu_time'] = 3.e4
-    """ ### """
-    #options['model']['ground_station']['ddl_t_max'] = 95.04
-
-    options['user_options']['wind']['u_ref'] = w
-
-    options['nlp']['n_k'] = 60
-    #options['model']['system_bounds']['u']['dkappa'] = [-1.0, 1.0]
-
-    #options['model']['system_bounds']['xd']['l_t'] = [1.0e-2, 1.0e3]
-    #options['solver']['initialization']['xd']['l_t'] = 1.0e3 # initial guess
-
-    # don't include induction effects, use simple tether drag
-    options['user_options']['induction_model'] = 'not_in_use'
-    options['user_options']['tether_drag_model'] = 'split'
-
-    # initial tether length guess
-    #options['solver']['initialization']['xd']['l_t'] = 200.0 # initial guess
-
-    ##################
-    # OPTIMIZE TRIAL #
-    ##################
-
-    options['solver']['linear_solver'] = 'ma57'
-    #options['solver']['initialization']['fix_tether_length'] = True
-
-    # initialize and optimize trial
-    trial = awe.Trial(options, name)
-    trial.build()
-    trial.optimize()
-    trial.quality.print_results()
-    quality_print_results = trial.quality.return_results()
-    trial.plot('level_3')
-    trial.write_to_csv()
-    solve_succed(quality_print_results, name)
-    #pdb.set_trace()
-
-    V_final = trial.optimization.V_final
-    V_solution_scaled = trial.nlp.V(trial.optimization.solution['x'])
-    print(V_final['xd', :, 'i_sd'],V_final['xd', :, 'i_sq'])
-    print(V_solution_scaled['xd', :, 'i_sd'],V_solution_scaled['xd', :, 'i_sq'])
+"""
